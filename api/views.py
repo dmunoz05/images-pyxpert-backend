8f432c667fa88c827021da27a442fac92d1a735a
= <daniel20025febrero29@gmail.com>
2024-04-04 15:11:38 -0500

    Firs functions for search contourns

--- api/views.py
+++ api/views.py
@@ -12,7 +12,7 @@ import urllib.request
 import numpy as np
 import base64
 import cv2
-
+import matplotlib.pyplot as plt
 
 # Create your views here.
 
@@ -41,7 +41,7 @@ class ProcessKeys(viewsets.ModelViewSet):
             else:
                 return JsonResponse({'status': 'error', 'message': 'Clave incorrecta'})
         except Exception as e:
-            return JsonResponse({'status': 'error', 'message': 'Error inesperado' })
+            return JsonResponse({'status': 'error', 'message': 'Error inesperado'})
 
     def get_key_ia(request):
         key_get = request.GET['key']
@@ -54,11 +54,77 @@ class ProcessKeys(viewsets.ModelViewSet):
             else:
                 return JsonResponse({'status': 'error', 'message': 'Clave incorrecta'})
         except Exception as e:
-            return JsonResponse({'status': 'error', 'message': 'Error inesperado' })
+            return JsonResponse({'status': 'error', 'message': 'Error inesperado'})
 
 
 class ProcesImages(viewsets.ModelViewSet):
-    # devolver shape
+
+    def search_contourns_img(request):
+        if ('image_url' in request.GET):
+            image_url = request.GET['image_url']
+            try:
+
+                def draw(mask, color, textColor, img, ruta):
+                    contornos, _ = cv2.findContours(
+                        mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+                    for c in contornos:
+                        area = cv2.contourArea(c)
+                        if area > 2000:
+                            M = cv2.moments(c)
+                            if (M["m00"] == 0):
+                                M["m00"] = 1
+                            x = int(M["m10"]/M["m00"])
+                            y = int(M["m01"]/M["m00"])
+                            cv2.circle(img, (x, y), 7, (0, 255, 0), -1)
+                            font = cv2.FONT_HERSHEY_SIMPLEX
+                            cv2.putText(img, textColor, (x+10, y), font,
+                                        0.95, (0, 255, 0), 1, cv2.LINE_AA)
+                            newContourn = cv2.convexHull(c)
+                            cv2.drawContours(Img, [newContourn], 0, color, 3)
+                            # Añadir contorno
+                            listSingleContourn.append(
+                                {"contorno": newContourn, "Ruta": ruta})
+
+                # Procesando la imagen
+                RuteImg = 'dorito13.jpg'
+                Img = cv2.imread(RuteImg)
+                imgGray = cv2.cvtColor(Img, cv2.COLOR_BGR2GRAY)
+                Imagen_hsv = cv2.cvtColor(Img, cv2.COLOR_BGR2HSV)
+                ImgResize = cv2.resize(np.uint8(imgGray), (50, 50))
+
+                # Detectar contornos
+                listSingleContourn = []
+
+                # Colores HSV
+                # Naranja
+                orangeBajo = np.array([5, 100, 20], np.uint8)
+                orangeAlto = np.array([15, 255, 255], np.uint8)
+
+                # Amarillo
+                amarilloBajo = np.array([15, 100, 20], np.uint8)
+                amarilloAlto = np.array([45, 255, 255], np.uint8)
+
+                # Leer imagen
+                frameHSV = cv2.cvtColor(Img, cv2.COLOR_BGR2HSV)
+
+                # Mascara naranja y amarillo
+                maskOrange = cv2.inRange(frameHSV, orangeBajo, orangeAlto)
+                maskYellow = cv2.inRange(frameHSV, amarilloBajo, amarilloAlto)
+                maskOrangeAndYellow = cv2.add(maskOrange, maskYellow)
+
+                # Dibujamos los contornos
+                draw(maskOrangeAndYellow, (0, 165, 255), 'Centro', Img, RuteImg)
+
+                # Procesar imagen
+                # plt.imshow(Img, cmap='gray')
+                # plt.title('Imagen en escala de grises')
+                # plt.axis('off')
+                # plt.show()
+
+                return JsonResponse({'status': 'success', 'image': Img})
+            except Exception as e:
+                return JsonResponse({'status': 'error', 'message': 'Error al procesar la imagen: {}'.format(str(e))})
+
     def get_shape(request):
         if 'image_url' in request.GET:
             image_url = request.GET['image_url']
@@ -67,6 +133,18 @@ class ProcesImages(viewsets.ModelViewSet):
                 arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
                 image = cv2.imdecode(arr, -1)
                 return JsonResponse({'status': 'success', 'shape': image.shape})
+            except Exception as e:
+                return JsonResponse({'status': 'error', 'message': 'Error al procesar la imagen: {}'.format(str(e))})
+
+    def get_rgb_google(request):
+        if ('image_url' in request.GET):
+            image_url = request.GET['image_url']
+            try:
+                req = urllib.request.urlopen(image_url)
+                arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
+                image = cv2.imdecode(arr, -1)
+                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                return JsonResponse({'status': 'success', 'image': image})
             except Exception as e:
                 return JsonResponse({'status': 'error', 'message': 'Error al procesar la imagen: {}'.format(str(e))})
 
