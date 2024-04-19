ff7de628fa30ce9d47633b4cc94769bd10ee66df
= <daniel20025febrero29@gmail.com>
2024-04-18 21:17:43 -0500

    Get characteristics endpoint

--- api/views.py
+++ api/views.py
@@ -49,12 +49,61 @@ class ProcessKeys(viewsets.ModelViewSet):
 class ProcessImages(viewsets.ModelViewSet):
 
     def characteristic_image_google(request):
-        key_get = request.GET['key']
-        try:
+        if 'image_url' in request.GET:
+            image_url = request.GET['image_url']
 
-            return JsonResponse({'status': 'error', 'message': 'Clave incorrecta'})
-        except Exception as e:
-            return JsonResponse({'status': 'error', 'message': 'Error inesperado'})
+            try:
+                # Leer imagen
+                req = urllib.request.urlopen(image_url)
+                arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
+                Img = cv2.imdecode(arr, -1)
+
+                # Procesando la imagen
+                Gris = cv2.cvtColor(Img, cv2.COLOR_BGR2GRAY)
+                Grisinverted2 = cv2.bitwise_not(Gris)
+                Bin = Grisinverted2 > 100
+                Bin = cv2.resize(np.uint8(Bin), (50, 50))
+
+                # Encontrar los contornos de la imagen
+                contours, _ = cv2.findContours(
+                    np.uint8(Bin), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+                if len(contours) > 0:
+                    # Tomar solo el contorno más grande (puede haber varios)
+                    largest_contour = max(contours, key=cv2.contourArea)
+
+                # Calcular área
+                area = cv2.contourArea(largest_contour)
+
+                # Calcular perímetro
+                perimeter = cv2.arcLength(largest_contour, closed=True)
+
+                # Calcular el centro de masa
+                M = cv2.moments(largest_contour)
+                center_x = int(M['m10'] / M['m00'])
+                center_y = int(M['m01'] / M['m00'])
+
+                # Calcular la circularidad
+                circularity = (4 * np.pi * area) / (perimeter ** 2)
+
+                # Calcular la elipticidad
+                _, (major_axis, minor_axis), _ = cv2.fitEllipse(
+                    largest_contour)
+                ellipticity = major_axis / minor_axis
+
+                # Calcular los momentos de Hu
+                Hu_moments = np.transpose(
+                    cv2.HuMoments(M))  # Siete valores
+
+                # Agregando alto y ancho
+                P1, P2, Width, Height = cv2.boundingRect(np.uint8(Bin))
+
+                data = {'area': area, 'perimeter': perimeter, 'ellipticity': ellipticity, 'center_x': center_x,
+                        'center_y': center_y, 'circularity': circularity, 'Hu_moments': Hu_moments.tolist(), 'Height': Height, 'Width': Width}
+
+                return JsonResponse({'status': 200, 'characteristics': data})
+            except Exception as e:
+                return JsonResponse({'status': 'error', 'message': e.args})
 
     @csrf_exempt
     def characteristic_image_pc(request):
@@ -220,8 +269,129 @@ class ProcessImages(viewsets.ModelViewSet):
             except Exception as e:
                 return JsonResponse({'status': 'error', 'message': e.args})
 
+    def search_contourns_img_google(request):
+        if request.method == 'POST':
+            try:
+                # Leer la imagen del cuerpo de la solicitud
+                body_unicode = request.body.decode('utf-8')
+                body_data = json.loads(body_unicode)
+                image_base64 = body_data.get('image')
+                if image_base64:
+                    decode_image = base64.b64decode(image_base64)
+                    # Decodificar la imagen usando OpenCV
+                    im_arr = np.frombuffer(decode_image, dtype=np.uint8)
+                    Img = cv2.imdecode(im_arr, -1)
+
+                    def draw(mask, color, textColor, img):
+                        contornos, _ = cv2.findContours(
+                            mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+                        for c in contornos:
+                            area = cv2.contourArea(c)
+                            if area > 2000:
+                                M = cv2.moments(c)
+                                if (M["m00"] == 0):
+                                    M["m00"] = 1
+                                x = int(M["m10"]/M["m00"])
+                                y = int(M["m01"]/M["m00"])
+                                cv2.circle(img, (x, y), 7, (0, 255, 0), -1)
+                                font = cv2.FONT_HERSHEY_SIMPLEX
+                                cv2.putText(img, textColor, (x+10, y),
+                                            font, 0.95, (0, 255, 0), 1, cv2.LINE_AA)
+                                newContourn = cv2.convexHull(c)
+                                cv2.drawContours(
+                                    Img, [newContourn], 0, color, 3)
+
+                    # Colores HSV
+
+                    # Rojo
+                    redBajo1 = np.array([0, 100, 20], np.uint8)
+                    redAlto1 = np.array([5, 255, 255], np.uint8)
+                    redBajo2 = np.array([175, 100, 20], np.uint8)
+                    redAlto2 = np.array([180, 255, 255], np.uint8)
+
+                    # Naranja
+                    orangeBajo = np.array([5, 100, 20], np.uint8)
+                    orangeAlto = np.array([15, 255, 255], np.uint8)
+
+                    # Amarillo
+                    amarilloBajo = np.array([15, 100, 20], np.uint8)
+                    amarilloAlto = np.array([45, 255, 255], np.uint8)
+
+                    # Verde
+                    verdeBajo = np.array([45, 100, 20], np.uint8)
+                    verdeAlto = np.array([85, 255, 255], np.uint8)
+
+                    # Azul claro
+                    azulBajo1 = np.array([100, 100, 20], np.uint8)
+                    azulAlto1 = np.array([125, 255, 255], np.uint8)
+
+                    # Azul oscuro
+                    azulBajo2 = np.array([125, 100, 20], np.uint8)
+                    azulAlto2 = np.array([130, 255, 255], np.uint8)
+
+                    # Morado
+                    moradoBajo = np.array([135, 100, 20], np.uint8)
+                    moradoAlto = np.array([145, 255, 255], np.uint8)
+
+                    # Violeta
+                    violetaBajo = np.array([145, 100, 20], np.uint8)
+                    violetaAlto = np.array([170, 255, 255], np.uint8)
+
+                    frameHSV = cv2.cvtColor(Img, cv2.COLOR_BGR2HSV)
+                    # Detectamos los colores
+
+                    # Rojo
+                    maskRed1 = cv2.inRange(frameHSV, redBajo1, redAlto1)
+                    maskRed2 = cv2.inRange(frameHSV, redBajo2, redAlto2)
+                    maskRed = cv2.add(maskRed1, maskRed2)
+
+                    # Naranja
+                    maskOrange = cv2.inRange(frameHSV, orangeBajo, orangeAlto)
+
+                    # Amarillo
+                    maskAmarillo = cv2.inRange(
+                        frameHSV, amarilloBajo, amarilloAlto)
+
+                    # Verde
+                    maskVerde = cv2.inRange(frameHSV, verdeBajo, verdeAlto)
+
+                    # Azul
+                    maskAzul1 = cv2.inRange(frameHSV, azulBajo1, azulAlto1)
+                    maskAzul2 = cv2.inRange(frameHSV, azulBajo2, azulAlto2)
+                    maskAzul = cv2.add(maskAzul1, maskAzul2)
+
+                    # Morado
+                    maskMorado = cv2.inRange(frameHSV, moradoBajo, moradoAlto)
+
+                    # Violeta
+                    maskVioleta = cv2.inRange(
+                        frameHSV, violetaBajo, violetaAlto)
+
+                    # Dibujamos los contornos
+                    draw(maskRed, (0, 0, 255), 'Rojo', Img)
+                    draw(maskOrange, (0, 165, 255), 'Naranja', Img)
+                    draw(maskAmarillo, (0, 255, 255), 'Amarillo', Img)
+                    draw(maskVerde, (0, 255, 0), 'Verde', Img)
+                    draw(maskAzul, (255, 0, 0), 'Azul', Img)
+                    draw(maskMorado, (255, 0, 255), 'Morado', Img)
+                    draw(maskVioleta, (255, 0, 255), 'Violeta', Img)
+                    # draw(maskOrangeAndYellow, (0, 165, 255), 'Centro', Img)
+
+                    buffer = BytesIO()
+
+                    # Convertir la imagen a BytesIO
+                    Image.fromarray(Img).save(buffer, format='PNG')
+
+                    # Obtener los datos de la imagen
+                    image_data = buffer.getvalue()
+
+                    # Aquí devuelvo un mensaje JSON indicando que la imagen ha sido procesada
+                return HttpResponse(image_data, content_type="image/png")
+            except Exception as e:
+                return JsonResponse({'status': 'error', 'message': 'Error al procesar la imagen: {}'.format(str(e))})
+
     @ csrf_exempt
-    def search_contourns_with_color_img_pc(request):
+    def search_contourns_with_colo_img_pc(request):
         if request.method == 'POST':
             try:
                 # Leer la imagen del cuerpo de la solicitud
